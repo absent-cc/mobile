d394d713e414199be44a777cbb5b22e875308b50
Leah Vashevko <leah@vashevko.com>
2022-04-22 20:39:50 -0400

    Update build scripts

--- scripts/ps/publish/publish_prod.ps1
+++ scripts/ps/publish/publish_prod.ps1
@@ -1,6 +1,6 @@
 $oldValue = $Env:APP_VARIANT
 $Env:APP_VARIANT = "production"
 
-expo publish --release-channel default
+expo publish --release-channel "production-v2"
 
 $Env:APP_VARIANT = $oldValue
