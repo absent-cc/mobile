b4a268b52b0a370b1ccc6aa12b5db62834418e53
Leah Vashevko <leah@vashevko.com>
2022-10-25 00:39:24 -0400

    Remove unused import

--- src/theme/SeasonalThemes.tsx
+++ src/theme/SeasonalThemes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import Video from 'react-native-video';
-import { ThemeOption, Themes, ThemeType } from './Themes';
+import { ThemeOption, ThemeType } from './Themes';
 
 export enum EditableElement {
     Animation = 'Animation',
